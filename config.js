cd5de5f60d206a446f3d876cd88bf333d74d1eb5
Jose Contreras <jcontrerasp2@est.ups.edu.ec>
2020-10-02 19:54:20 -0500

    Actualizacon final para entrega2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.a4o7l.gcp.mongodb.net/UTUPS?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.jirrd.gcp.mongodb.net/UTUPS?retryWrites=true&w=majority',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
